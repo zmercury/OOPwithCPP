66f92277bfa7ed9091dcf42ec3a15ecdeb18f2cd
Vijaya Malla <mallabiju@gmail.com>
2022-11-09 07:31:59 +0545

    user input string and integers

---
+++ 1_Basic_IO/b_addnums.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 67, b = 79;
+    int sum = a+b;
+    cout<<"The sum is "<<sum;
+    return 0;
+}
---
+++ 1_Basic_IO/c_userinputstring.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string fname;
+    cout << "Enter your name";
+    cin >> fname;
+    cout <<"Your name is " << fname;
+    return 0;
+}
